95379f0eeab4aba7157a8e07d677f6912ef4c5ed
Mcodes23 <mateliboniface@gmail.com>
2025-11-05 12:47:13 +0300

    Hot fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./index.css";
 import Header from "./components/header/Header";
 import Hero from "./components/hero/Hero";
 import About from "./components/about/About";
-import Skills from "./components/skills/skills";
+import Skills from "./components/skills/Skills";
 import Projects from "./components/projects/projects";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
