792d2f07b01667583adad09ba0faa8d016dbb3e7
Mcodes23 <mateliboniface@gmail.com>
2025-12-14 08:49:56 +0300

    revert changes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Change base to '/' so it works on Vercel's root domain
-  base: "/",
+  // eslint-disable-next-line no-undef
+  base: process.env.VITE_BASE_PATH || "/my-portfolio-app",
 });
