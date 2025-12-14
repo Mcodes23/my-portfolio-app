7de0f20b53a2509f9634e6e19bb384c2d1f1efc8
Mcodes23 <mateliboniface@gmail.com>
2025-12-14 08:33:33 +0300

    deployment fixes

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // eslint-disable-next-line no-undef
-  base: process.env.VITE_BASE_PATH || "/my-portfolio-app"
-})
+  // Change base to '/' so it works on Vercel's root domain
+  base: "/",
+  build: {
+    // Force Vite to output to "build" instead of "dist" to match Vercel defaults
+    outDir: "build",
+  },
+});
